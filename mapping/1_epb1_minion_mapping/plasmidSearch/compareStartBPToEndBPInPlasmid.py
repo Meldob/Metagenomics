56380cfb9fef2b8b52ab69dcdf1b2b2ce40e0ff7
Meldob <23016272+Meldob@users.noreply.github.com>
2019-01-04 10:45:25 +1000

    Updated CompareStartBPToEndBPInPlasmid - Still  Broken

--- mapping/1_epb1_minion_mapping/plasmidSearch/compareStartBPToEndBPInPlasmid.py
+++ mapping/1_epb1_minion_mapping/plasmidSearch/compareStartBPToEndBPInPlasmid.py
@@ -8,11 +8,11 @@
 #reverseFileFirst500 = 'compareStartBPToEndBPInPlasmid_TestText2.txt'
 
 #BRASSICA PLASMID TEST
-#filePath = 'brassica_napus_plasmid.fixed.bam'
-#forwardFileFirst500 = '/final.contigs.fa.brassica_napus_plasmid.fixed.1.fq_1st500_1.sam'
-#forwardFileLast500 = '/final.contigs.fa.brassica_napus_plasmid.fixed.1.fq_last500_1.sam'
-#reverseFileLast500 = '/final.contigs.fa.brassica_napus_plasmid.fixed.1.fq_last500_2.sam'
-#reverseFileFirst500 = '/final.contigs.fa.brassica_napus_plasmid.fixed.1.fq_1st500_2.sam'
+filePath = 'brassica_napus_plasmid.fixed.bam'
+forwardFileFirst500 = '/final.contigs.fa.brassica_napus_plasmid.fixed.1.fq_1st500_1.sam'
+forwardFileLast500 = '/final.contigs.fa.brassica_napus_plasmid.fixed.1.fq_last500_1.sam'
+reverseFileLast500 = '/final.contigs.fa.brassica_napus_plasmid.fixed.1.fq_last500_2.sam'
+reverseFileFirst500 = '/final.contigs.fa.brassica_napus_plasmid.fixed.1.fq_1st500_2.sam'
 
 #NOSTOC PLASMID TEST
 #filePath = 'nostoc_azollae_plasmid.fixed.bam'
@@ -23,11 +23,11 @@
 
 
 #ACETOBACTER PLASMID TEST
-filePath = 'AcetobacterPasteurianus.bam'
-forwardFileFirst500 = '/final.contigs.fa.AcetobacterPasteurianus.1.fq_1st500_1.sam'
-forwardFileLast500 = '/final.contigs.fa.AcetobacterPasteurianus.1.fq_last500_1.sam'
-reverseFileLast500 = '/final.contigs.fa.AcetobacterPasteurianus.1.fq_last500_2.sam'
-reverseFileFirst500 = '/final.contigs.fa.AcetobacterPasteurianus.1.fq_1st500_2.sam'
+#filePath = 'AcetobacterPasteurianus.bam'
+#forwardFileFirst500 = '/final.contigs.fa.AcetobacterPasteurianus.1.fq_1st500_1.sam'
+#forwardFileLast500 = '/final.contigs.fa.AcetobacterPasteurianus.1.fq_last500_1.sam'
+#reverseFileLast500 = '/final.contigs.fa.AcetobacterPasteurianus.1.fq_last500_2.sam'
+#reverseFileFirst500 = '/final.contigs.fa.AcetobacterPasteurianus.1.fq_1st500_2.sam'
 
 
 
