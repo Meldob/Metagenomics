fbf4204519bfea41fbfb79237bd1e4882d362fcb
Meldob <23016272+Meldob@users.noreply.github.com>
2019-01-08 14:50:56 +1000

    updated findPlasmidsInBamFile.py by finding reference names through pysam

--- mapping/1_epb1_minion_mapping/plasmidSearch/findPlasmidsInBamFile.py
+++ mapping/1_epb1_minion_mapping/plasmidSearch/findPlasmidsInBamFile.py
@@ -10,12 +10,38 @@ import pysam
 #opens bam file for reading
 samfile = pysam.AlignmentFile(inputFile, "rb")
 
-#iterate through lines
-for read in samfile:
-    #print(read)
+#create list to store names of all contigs
+contigRefList = []
+#iterate through lines to find all contigs
+for line in samfile:
+    contigIdentifier = line.reference_name    
+    if contigIdentifier not in contigRefList:
+        contigRefList.append(contigIdentifier)
+        contigLength = samfile.lengths[samfile.get_tid(contigIdentifier)]
+        print(contigIdentifier)
+        print(contigLength)
+        
+        
+
+        
+#        line.get_reference_length(self, ref)
+#print(contigRefList)   
+
+
+#for ref in contigRefList:
+#    print(ref)
+#    print(line.get_reference_length(self, ref))
+    #iter = samfile.fetch(str(ref), 1, 500)
+    #for x in iter:
+    #    print (str(x))
+        
+    
+    
+    
+    #import IPython; IPython.embed()
     #create a list of each of the values in the line containing a forward identifier, separated by a tab
-    values = read.split("\t")
-    print(values[0])
+    #values = read.split("\t")
+    #print(values[0])
     #contigIdentifier = values[3].strip
     #print contigIdentifier
 #find contig name
@@ -27,7 +53,7 @@ for read in samfile:
 #for x in iter:
 #    print (str(x))
     
-    
+
 samfile.close
 
 
