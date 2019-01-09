2fb0c016b566da4fb98e7bd62f33281e2ef020eb
Meldob <23016272+Meldob@users.noreply.github.com>
2019-01-09 14:38:37 +1000

    fixed the findPlasmidsInBamFile script and combined it with compareStartToEndBPInPlasmid.py to automate this entire process.

--- mapping/1_epb1_minion_mapping/plasmidSearch/findPlasmidsInBamFile2.py
+++ mapping/1_epb1_minion_mapping/plasmidSearch/findPlasmidsInBamFile2.py
@@ -59,89 +59,48 @@ for line in samfile:
             print('Last 500bp, second read, forward mapping list is: ' + str(last5002nd1ReadList))
             print('')
             #starts count for number of matches
-            count = 0
-            #creates list for identifiers
-            identifiers = ['']
+            count_1st5001stVSlast5002nd = 0
+            count_last5001stVS1st5002nd = 0
+            
             #creates list for matching identifiers
-            matchingIdentifiers = []
-
+            matches_1st5001stVSlast5002nd = []
+            matches_last5001stVS1st5002nd = []
             #for each entry in the first read lists, find if it matches to a corresponding second read
-            matches = []
             for firstRead in first5001st2ReadList:
                 for secondRead in last5002nd1ReadList:
                     if firstRead == secondRead:
                         count =+ 1
                         print("It's a match, the identifier is: " + firstRead)
-                        matches.append(firstRead)
+                        matches_1st5001stVSlast5002nd.append(firstRead)
             for firstRead in last5001st1ReadList:
                 for secondRead in first5002nd2ReadList:
                     if firstRead == secondRead:
                         count =+ 1
                         print("It's a match, the identifier is: " + firstRead)
-                        matches.append(firstRead)            
+                        matches_last5001stVS1st5002nd.append(firstRead)            
             #If there are no matches, report this
-            if count == 0:
+            if count_1st5001stVSlast5002nd == 0:
                 print("")
-                print("There were no matches from the " + testName + " match test")
+                print("There were no matches comparing first reads from the first 500bp with the 2nd reads form the last 500bp")
                 print("")
             #print the matching list of identifiers
             else:
-                print("The matching identifiers of the " + testName + " match test were " + str(matchingIdentifiers))
+                print("The matching identifiers when comparing first reads from the first 500bp with the 2nd reads from the last 500bp are: " + str(matches_1st5001stVSlast5002nd))
                 print("")
                 print("")
+            #If there are no matches, report this
+            if count_last5001stVS1st5002nd == 0:
+                print("")
+                print("There were no matches comparing first reads from the last 500bp with the 2nd reads from the first 500bp")
+                print("")
+            #print the matching list of identifiers
+            else:
+                print("The matching identifiers when comparing first reads from the last 500bp with the 2nd reads from the first 500bp are: " + str(matches_1st5001stVSlast5002nd))
+                print("")
+                print("")    
+            
 
 samfile.close
-
-### COMPARE FORWARD AND REVERSE READS TO SEE IF THERE IS OVERLAP
-
-
-
-
-
-#for each second read in the forward direction of the last 500bp of the original sam file, find if any of these reads match 1st reads in the reverse direction of the first 500bp of the original sam file
-#start by finding and recording the forward read identifier in the list
-for line in inputFile:
-    #create an empty variable for the forward read identifier, find if the line has an identifier, and the starting point of this identifier
-    forwardReadIdentifier = ''
-    startForwardReadIdentifier = line.find('N')
-    #test
-    #print startForwardReadIdentifier
-    if line[0] == 'N':
-        #create a list of each of the values in the line containing a forward identifier, separated by a tab
-        forwardValues = line.split("\t")
-        #assuming the first value in the list is the forward read identifier, strip the whitespace and then add this value to the list of identifiers
-        forwardReadIdentifier = forwardValues[0].strip()
-        identifiers.append(forwardReadIdentifier)
-        #test
-        #print("forward read identifier is" + forwardReadIdentifier)
-        #open a pre-prepared sam file, that has included only the reverse reads in the first 500bp of the final assembled contig
-        with open(fullFilePathFirst500ReverseFirstRead) as inputFile2:
-            for line in inputFile2:
-                #create an empty variable for the reverse read identifier, find if the line has an identifier
-                reverseReadIdentifier = 'reverseRead'
-                if line[0] == 'N':
-                    reverseValues = line.split("\t")
-                    #assuming the first value in the list is the reverse read identifier, strip the whitespace and then identify this as the reverseReadIdentifier
-                    reverseReadIdentifier = reverseValues[0].strip()
-                    #test
-                    #print("reverse read identifier is" + reverseReadIdentifier)
-                    #if the forward read identifier from inputFile is the same as the reverse read identifier from inputFile2, then print that they match and add the identifier to the matching Identifiers list
-                    if forwardReadIdentifier == reverseReadIdentifier:
-                        print("It's a match, the identifier is: " + forwardReadIdentifier)
-                        matchingIdentifiers.append(forwardReadIdentifier)
-                        #increase count of matches by 1
-                        count += 1 
-
-#If there are no matches, report this
-if count == 0:
-    print("")
-    print("There were no matches from the " + testName + " match test")
-    print("")
-#print the matching list of identifiers
-else:
-    print("The matching identifiers of the " + testName + " match test were " + str(matchingIdentifiers))
-    print("")
-    print("")
 
 
 
