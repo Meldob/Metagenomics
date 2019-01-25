1d5399eeafe623d41eb812bff8e0a247e12695a6
Meldob <23016272+Meldob@users.noreply.github.com>
2019-01-25 16:02:42 +1000

    fixing the file paths, still broken

--- mapping/1_epb1_minion_mapping/plasmidSearch/findPlasmidScript/test/findPlasmidsTest.py
+++ mapping/1_epb1_minion_mapping/plasmidSearch/findPlasmidScript/test/findPlasmidsTest.py
@@ -8,13 +8,13 @@ import unittest
 import extern
 import os.path
 
-path_to_script = './findPlasmidsInBamFile.py'
+path_to_script = 'srv/projects/abisko/melody/mapping/1_epb1_minion_mapping/plasmidSearch/findPlasmidScript/findPlasmidsInBamFile/findPlasmidsInBamFile.py'
 
 class TestStringMethods(unittest.TestCase):
     
     
     def testBrassica(self):
-        brassicaCommand = path_to_script + ' -i brassica_napus_plasmid.fixed.bam/final.contigs.fa.brassica_napus_plasmid.fixed.1.fq.bam -o /dev/stdout'
+        brassicaCommand = path_to_script + ' -i srv/projects/abisko/melody/mapping/1_epb1_minion_mapping/plasmidSearch/findPlasmidScript/test/data/final.contigs.fa.brassica_napus_plasmid.fixed.1.fq.bam -o /dev/stdout'
         brassicaContents = (extern.run(brassicaCommand)).strip()
         
         expectedBrassicaOutput = "contigName" + "\t" + "numberOfMatches" + "\t" + "matchingReads" + "\n" + "k99_1" + "\t" + "33" + "\t" +"['NC_016583.1_911_1381_2:0:0_3:0:0_3c', 'NC_016583.1_914_1401_1:0:0_1:0:0_8c', 'NC_016583.1_866_1302_7:0:0_5:0:0_c4', 'NC_016583.1_865_1343_3:0:0_4:0:0_1b', 'NC_016583.1_811_1353_2:0:0_3:0:0_38', 'NC_016583.1_797_1311_7:0:0_7:0:0_df', 'NC_016583.1_784_1255_4:0:0_3:0:0_48', 'NC_016583.1_766_1233_0:0:0_5:0:0_35', 'NC_016583.1_751_1329_3:0:0_2:0:0_90', 'NC_016583.1_729_1239_3:0:0_6:0:0_25', 'NC_016583.1_701_1212_2:0:0_4:0:0_ce', 'NC_016583.1_698_1190_3:0:0_3:0:0_84', 'NC_016583.1_690_1134_5:0:0_0:0:0_d0', 'NC_016583.1_684_1129_4:0:0_8:0:0_ae', 'NC_016583.1_683_1201_4:0:0_1:0:0_b8', 'NC_016583.1_676_1213_5:0:0_3:0:0_55', 'NC_016583.1_619_1076_4:1:0_7:0:0_cf', 'NC_016583.1_918_1409_1:0:0_3:0:0_e', 'NC_016583.1_896_1422_2:0:0_6:0:0_de', 'NC_016583.1_888_1365_3:0:0_5:0:0_65', 'NC_016583.1_886_1386_2:0:0_3:0:0_a7', 'NC_016583.1_874_1418_2:0:0_3:0:0_1a', 'NC_016583.1_867_1401_5:0:0_2:0:0_64', 'NC_016583.1_844_1349_2:0:0_3:0:0_3e', 'NC_016583.1_772_1286_3:0:0_2:0:0_47', 'NC_016583.1_757_1280_8:0:0_2:0:0_61', 'NC_016583.1_725_1191_2:0:0_5:0:0_32', 'NC_016583.1_656_1133_4:0:0_4:0:0_ad', 'NC_016583.1_651_1160_2:1:0_1:0:0_ac', 'NC_016583.1_622_1182_2:1:0_3:0:0_b6', 'NC_016583.1_613_1091_0:1:0_4:0:0_10', 'NC_016583.1_567_1140_2:1:0_1:0:0_78', 'NC_016583.1_550_1076_2:1:0_3:0:0_28']"
@@ -31,7 +31,7 @@ class TestStringMethods(unittest.TestCase):
     def testNostoc(self):        
         #set the output file
         #set the command to check
-        nostocCommand = path_to_script + ' -i nostoc_azollae_plasmid.fixed.bam/final.contigs.fa.nostoc_azollae_plasmid.fixed.1.fq.bam -o /dev/stdout'
+        nostocCommand = path_to_script + ' -i srv/projects/abisko/melody/mapping/1_epb1_minion_mapping/plasmidSearch/findPlasmidScript/test/data/final.contigs.fa.nostoc_azollae_plasmid.fixed.1.fq.bam -o /dev/stdout'
         # run the test command and put the output into the variable nostocContents
         nostocContents = (extern.run(nostocCommand)).strip()
         
@@ -47,7 +47,7 @@ class TestStringMethods(unittest.TestCase):
         self.assertEqual(expectedNostocOutput, nostocContents)
     
     def testAcetobacter(self):
-        acetoCommand = path_to_script + ' -i AcetobacterPasteurianus.bam/final.contigs.fa.AcetobacterPasteurianus.1.fq.bam -o /dev/stdout'
+        acetoCommand = path_to_script + ' -i srv/projects/abisko/melody/mapping/1_epb1_minion_mapping/plasmidSearch/findPlasmidScript/test/data/final.contigs.fa.AcetobacterPasteurianus.1.fq.bam -o /dev/stdout'
         acetoContents = (extern.run(acetoCommand)).strip()
         
         expectedAcetoOutput = "contigName" + "\t" + "numberOfMatches" + "\t" + "matchingReads" + "\n" + "k141_103" + "\t" + "0" + "\t" + "[]k141_106" + "\t" + "0" + "\t" + "[]k141_153" + "\t" + "0" + "\t" + "[]k141_157" + "\t" + "0" + "\t" + "[]k141_164" + "\t" + "0" + "\t" + "[]k141_177" + "\t" + "0" + "\t" + "[]k141_183" + "\t" + "0" + "\t" + "[]k141_184" + "\t" + "0" + "\t" + "[]k141_186" + "\t" + "0" + "\t" + "[]k141_187" + "\t" + "0" + "\t" + "[]k141_196" + "\t" + "0" + "\t" + "[]k141_203" + "\t" + "0" + "\t" + "[]k141_210" + "\t" + "0" + "\t" + "[]k141_213" + "\t" + "0" + "\t" + "[]k141_230" + "\t" + "0" + "\t" + "[]k141_232" + "\t" + "0" + "\t" + "[]k141_236" + "\t" + "0" + "\t" + "[]k141_237" + "\t" + "0" + "\t" + "[]k141_238" + "\t" + "0" + "\t" + "[]k141_240" + "\t" + "0" + "\t" + "[]k141_241" + "\t" + "0" + "\t" + "[]k141_243" + "\t" + "0" + "\t" + "[]k141_246" + "\t" + "0" + "\t" + "[]k141_247" + "\t" + "0" + "\t" + "[]k141_249" + "\t" + "0" + "\t" + "[]k141_253" + "\t" + "0" + "\t" + "[]k141_254" + "\t" + "0" + "\t" + "[]k141_256" + "\t" + "0" + "\t" + "[]k141_257" + "\t" + "0" + "\t" + "[]"
